0fd9a37942991451a654ec6efab32204549e490d
LaFlame-03 <derickfelipe2003@gmail.com>
2023-06-16 22:14:03 -0300

    adicionando classes java e parametros do addComp

---
+++ src/java/controles/ComponentesDAO.java
@@ -0,0 +1,43 @@
+//Derick felipe
+package controles;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Date;
+public class ComponentesDAO{
+    
+//Classes boolean para estabelecer os metodos de conexao DAO de cada ação
+public boolean addComp(Componentes p_comp) throws ClassNotFoundException{ 
+    //INSERT INTO componentes(data_hora,switch, porta_switch, patch_painel, porta_patchpainel, end_mac, nome_pc, end_ip, estado, obs)
+    String sql_INSERT = "INSERT INTO componentes(data_hora,switch, porta_switch, patch_painel,porta_patchpainel, end_mac, nome_pc, end_ip, estado, obs)VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    try {
+        Connection conn = ConexaoBancoDAO.conectar();
+      
+        PreparedStatement sql_comando = conn.prepareStatement(sql_INSERT);
+        
+        
+        sql_comando.setDate(1, new Date(p_comp.getData().getTime())); // Define a data usando java.sql.Date
+        sql_comando.setString(2, p_comp.getNomeSwitch());
+        sql_comando.setInt(3, p_comp.getPortaSwitch());
+        sql_comando.setString(4, p_comp.getPatchPanel());
+        sql_comando.setInt(5, p_comp.getPortaPatchPanel());
+        sql_comando.setString(6, p_comp.getEndMac());
+        sql_comando.setString(7, p_comp.getNomePC());
+        sql_comando.setString(8, p_comp.getEndIP());
+        sql_comando.setString(9, p_comp.getEstado());
+        sql_comando.setString(10, p_comp.getobservacao());
+
+
+        sql_comando.executeUpdate(); //executa comando SQL
+        conn.close();
+
+        return true;
+    } catch (SQLException ex) {
+         System.out.println("<br> Erro:" + ex);
+         return false;
+    }
+}
+
+}
