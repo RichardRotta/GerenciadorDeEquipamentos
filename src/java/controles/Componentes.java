ff5c05796f20a504d6a8a1215548c37963848b25
LaFlame-03 <derickfelipe2003@gmail.com>
2023-06-16 22:26:22 -0300

    ADICIONANDO CLASEES JAVA

--- src/java/controles/Componentes.java
+++ src/java/controles/Componentes.java
@@ -1,33 +1,65 @@
+//Derick felipe
 package controles;
 
 import java.util.Date;
 
-public class Produto {
+//Classe componentes:
+public class Componentes {
     //Atributos
-    private int cod;
-    private String nome;
-    private float valor;
-    private Date fabrica;
+    private int id;
+    private Date data;
+    private String nomeSwitch;
+    private int portaSwitch;
+    private String patchPanel;
+    private int portaPatchPanel;
+    private String endMac;
+    private String nomePC;
+    private String endIP;
+    private String estado;
+    private String observacao;
     
     //Métodos - Construtor
-    public Produto(int cod, String nome, float valor, Date fabrica) {
-        this.cod = cod;
-        this.nome = nome;
-        this.valor = valor;
-        this.fabrica = fabrica;
+    public Componentes(int id,Date data, String nomeSwitch, int portaSwitch, String patchPanel, int portaPatchPanel,
+                    String endMac,String nomePC, String endIP, String estado, String observacao) 
+    {
+        this.id = id;
+        this.data = data;
+        this.nomeSwitch = nomeSwitch;
+        this.portaSwitch = portaSwitch;
+        this.patchPanel = patchPanel;
+        this.portaPatchPanel = portaPatchPanel;
+        this.endMac = endMac;
+        this.nomePC = nomePC;
+        this.endIP = endIP;
+        this.estado = estado;
+        this.observacao = observacao;
     }
-    
-    // Métodos Setters       
-    public void setCod(int cod) {        this.cod = cod;    }
-    public void setNome(String nome) {        this.nome = nome;    }
-    public void setValor(float valor) {        this.valor = valor;    }
-    public void setFabrica(Date fabrica) {        this.fabrica = fabrica;    }
-
     // Métodos Getters
-    public int getCod() {        return cod;    }
-    public String getNome() {        return nome;    }
-    public float getValor() {        return valor;    }
-    public Date getFabrica() {        return fabrica;    } 
+    public int getId() {return id;}
+    public Date getData() {return data;} 
+    public String getNomeSwitch() {return nomeSwitch;}
+    public int getPortaSwitch() {return portaSwitch;}
+    public String getPatchPanel(){return patchPanel;}
+    public int getPortaPatchPanel() {return portaPatchPanel;}
+    public String getEndMac(){return endMac;}
+    public String getNomePC(){return nomePC;}
+    public String getEndIP(){return endIP;}
+    public String getEstado(){return estado;}
+    public String getobservacao(){return observacao;}
+
+    // Métodos Setters       
+    public void setId(int id) {this.id = id;}
+    public void setData(Date data) {this.data = data;}
+    public void setNomeSwitch(String nomeSwitch) {this.nomeSwitch = nomeSwitch;}
+    public void setPortaSwitch(int portaSwitch) {this.portaSwitch = portaSwitch;}
+    public void setPatchPanel(String patchPanel) {this.patchPanel = patchPanel;}
+    public void setPortaPatchPanel(int portaPatchPanel) {this.portaPatchPanel = portaPatchPanel;}
+    public void setEndMac(String endMac) {this.endMac = endMac;}
+    public void setNomePC(String nomePC) {this.nomePC = nomePC;}
+    public void setEndIP(String endIP) {this. endIP =  endIP;}
+    public void setEstado(String estado) {this.estado = estado;}
+    public void setobservacao(String observacao) {this.observacao = observacao;}
+
     
    
 }
